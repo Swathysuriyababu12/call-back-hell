f132e0075c31ee6222f98ee633202d6d4b5b22c5
Swathy <100306927+Swathysuriyababu12@users.noreply.github.com>
2022-07-28 22:25:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("p1").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("p1").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("p1").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("p1").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("p1").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("p1").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("p1").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("p1").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("p1").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("p1").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("p1").innerHTML =
+                        "HAPPY INDEPENDENCE DAY";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
